2081312fb01882ffa73109dc81ca8f7156f117be
Vitaly Pinchuk <pinchuk.vit.1@gmail.com>
2023-02-06 17:37:15 +0300

    Opera12 parseFromString supported now

--- xsl/config/any-fills.js
+++ xsl/config/any-fills.js
@@ -64,4 +64,41 @@
 			isFinite(value) &&
 			Math.floor(value) === value;
 	};
+
+
+	// @see https://stackoverflow.com/questions/9500318/#9500734
+	(function (DOMParser) {
+		"use strict";
+		var DOMParser_proto = DOMParser.prototype
+			, real_parseFromString = DOMParser_proto.parseFromString;
+
+		// Firefox/Opera/IE throw errors on unsupported types  
+		try {
+			// WebKit returns null on unsupported types  
+			if ((new DOMParser).parseFromString("", "text/html")) {
+				// text/html parsing is natively supported  
+				return;
+			}
+		} catch (ex) { }
+
+		DOMParser_proto.parseFromString = function (markup, type) {
+			if (/^\s*text\/html\s*(?:;|$)/i.test(type)) {
+				var doc = document.implementation.createHTMLDocument("")
+					, doc_elt = doc.documentElement
+					, first_elt;
+
+				doc_elt.innerHTML = markup;
+				first_elt = doc_elt.firstElementChild;
+
+				if (doc_elt.childElementCount === 1
+					&& first_elt.localName.toLowerCase() === "html") {
+					doc.replaceChild(first_elt, doc_elt);
+				}
+
+				return doc;
+			} else {
+				return real_parseFromString.apply(this, arguments);
+			}
+		};
+	}(DOMParser));
 })();
